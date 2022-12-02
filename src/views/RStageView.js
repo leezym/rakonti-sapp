22ff57cb234745e74b327aeff7654a38b590f72a
Martín Sierra <martinsg.9054@gmail.com>
2022-12-02 11:11:19 -0500

    Se implementó el patrón de diseño de interfaz gráfica de usuario Ventana Modal para mostrar la información general de la etapa seleccionada.

--- src/views/RStageView.js
+++ src/views/RStageView.js
@@ -6,20 +6,28 @@
  * 
  */
 
+import { useState } from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 import BackgroundImage from '../styled/BackgroundImage';
+import RModalStage from '../components/other/RModalStage';
 import RTip from '../components/stage/RTip';
 
 function RStageView() {
-  const currentStage = useSelector(state => state.story.currentStage);
+  const [showStageInfo, setShowStageInfo] = useState(false);
+  const currentStage = useSelector(state => state.story.currentStage); 
+
+  const onStageInfoClicked = (show = true) => {
+    setShowStageInfo(show);
+  }
 
   return <div>
     <BackgroundImage 
       src={currentStage.backgroundUrl} 
       alt='stage-background'/>
-    <TitleButton
+    <TitleButton 
+      onClick={onStageInfoClicked}
       left={currentStage.title.left}
       top={currentStage.title.top}
       width={currentStage.title.width}>
@@ -34,6 +42,10 @@ function RStageView() {
         <RTip key={tip.id} tip={tip}/> 
       ))
     }
+
+    { showStageInfo && <RModalStage 
+        onClose={onStageInfoClicked} 
+        item={currentStage}/> }
   </div>
 }
 
