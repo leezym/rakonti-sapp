2b684890336f26befacc5214e228ff8364cd4a9a
Martín Sierra <martinsg.9054@gmail.com>
2022-12-01 20:45:34 -0500

    Se actualizó la información relacionada al título y los consejos de cada etapa de la estructura narrativa El Viaje del Héroe, ubicados en el archivo narratives.js; se agregaron las imágenes de cada consejo; se implementó la interfaz visual para cada etapa de la estructura narrativa El Viaje del Héroe.

--- src/views/RStageView.js
+++ src/views/RStageView.js
@@ -6,10 +6,11 @@
  * 
  */
 
-//import styled from 'styled-components';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 import BackgroundImage from '../styled/BackgroundImage';
+import RTip from '../components/stage/RTip';
 
 function RStageView() {
   const currentStage = useSelector(state => state.story.currentStage);
@@ -18,7 +19,45 @@ function RStageView() {
     <BackgroundImage 
       src={currentStage.backgroundUrl} 
       alt='stage-background'/>
+    <TitleButton
+      left={currentStage.title.left}
+      top={currentStage.title.top}
+      width={currentStage.title.width}>
+      <Title color={currentStage.title.color}>
+        { currentStage.name }
+      </Title>
+    </TitleButton>
+    {
+      currentStage.tips.map(tip => (
+        <RTip key={tip.id} tip={tip}/> 
+      ))
+    }
   </div>
 }
+
+const Title = styled.h1` 
+  color: ${({ color }) => color}; 
+  font-size: 2.8em; 
+  font-weight: 450; 
+  height: 100%;
+  margin: 0;
+  text-align: center; 
+  width: 100%; 
+
+  @media (min-width: 1524px) {
+    font-size: 4em;
+  }
+`;
+
+const TitleButton = styled.button`
+  background-color: transparent; 
+  border: none; 
+  cursor: pointer; 
+  left: ${({ left }) => left}; 
+  padding: 0; 
+  position: absolute; 
+  top: ${({ top }) => top}; 
+  width: ${({ width }) => width}; 
+`;
 
 export default RStageView;
